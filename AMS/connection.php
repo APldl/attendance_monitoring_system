d41024be1c90218457bb7382ef0d5bedb87dbe04
APldl <adrianlopezdeleon@gmail.com>
2023-03-31 15:09:15 +0800

    Update connection.php

---
+++ AMS/connection.php
@@ -0,0 +1,11 @@
+<?php
+// Connect to MySQL database
+$servername = "localhost";
+$db_username = "root"; //xampp default
+$db_password = "";  //xampp default
+$database = "attendance_monitoring_system";
+
+  $conn = mysqli_connect($servername, $db_username, $db_password, $database);
+
+
+?>
